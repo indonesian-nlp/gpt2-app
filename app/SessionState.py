60ccfd22aaa978031ebcd8e1c929bc957aded033
Cahya Wirawan <cahya.wirawan@gmail.com>
2022-05-22 22:37:56 +0200

    removed s.enque

--- app/SessionState.py
+++ app/SessionState.py
@@ -76,13 +76,6 @@ def get(**kwargs):
     for session_info in session_infos:
         s = session_info.session
         if (
-            # Streamlit < 0.54.0
-            (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
-            or
-            # Streamlit >= 0.54.0
-            (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
-            or
-            # Streamlit >= 0.65.2
             (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
         ):
             this_session = s
